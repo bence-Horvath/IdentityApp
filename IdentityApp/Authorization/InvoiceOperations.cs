01a4754a4ce69d93f4abd892f2efbaaccf01c72b
bence <bence@BENCE-PC>
2023-05-14 19:51:29 +0100

    InvoiceCreatorAuthorizationHandler

--- IdentityApp/Authorization/InvoiceOperations.cs
+++ IdentityApp/Authorization/InvoiceOperations.cs
@@ -4,24 +4,24 @@ namespace IdentityApp.Authorization
 {
     public class InvoiceOperations
     {
-        public static OperationAuthorizationRequirement Create = new OperationAuthorizationRequirement { Name = Constants.CreateOperation };
-        public static OperationAuthorizationRequirement Read = new OperationAuthorizationRequirement { Name = Constants.ReadOperation };
-        public static OperationAuthorizationRequirement Update = new OperationAuthorizationRequirement { Name = Constants.UpdateOperation };
-        public static OperationAuthorizationRequirement Delete = new OperationAuthorizationRequirement { Name = Constants.DeleteOperation };
+        public static OperationAuthorizationRequirement Create = new OperationAuthorizationRequirement { Name = Constants.CreateOperationName };
+        public static OperationAuthorizationRequirement Read = new OperationAuthorizationRequirement { Name = Constants.ReadOperationName };
+        public static OperationAuthorizationRequirement Update = new OperationAuthorizationRequirement { Name = Constants.UpdateOperationName };
+        public static OperationAuthorizationRequirement Delete = new OperationAuthorizationRequirement { Name = Constants.DeleteOperationName };
 
-        public static OperationAuthorizationRequirement Approved = new OperationAuthorizationRequirement { Name = Constants.ApprovedOperation };
-        public static OperationAuthorizationRequirement Rejected = new OperationAuthorizationRequirement { Name = Constants.RejectedOperation };
+        public static OperationAuthorizationRequirement Approved = new OperationAuthorizationRequirement { Name = Constants.ApprovedOperationName };
+        public static OperationAuthorizationRequirement Rejected = new OperationAuthorizationRequirement { Name = Constants.RejectedOperationName };
 
     }
 
     public class Constants
     {
-        public static readonly string CreateOperation = "Create";
-        public static readonly string ReadOperation = "Read";
-        public static readonly string UpdateOperation = "Update";
-        public static readonly string DeleteOperation = "Delete";
+        public static readonly string CreateOperationName = "Create";
+        public static readonly string ReadOperationName = "Read";
+        public static readonly string UpdateOperationName = "Update";
+        public static readonly string DeleteOperationName = "Delete";
 
-        public static readonly string ApprovedOperation = "Approved";
-        public static readonly string RejectedOperation = "Rejected";
+        public static readonly string ApprovedOperationName = "Approved";
+        public static readonly string RejectedOperationName = "Rejected";
     }
 }
